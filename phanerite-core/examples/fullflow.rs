0ebffa6cc668d7bb9c1d5c9a43b64b5f468a903f
Tuxium <ursusfeline07@gmail.com>
2026-07-28 12:06:16 +0800

    Use url crate

--- phanerite-core/examples/fullflow.rs
+++ phanerite-core/examples/fullflow.rs
@@ -10,7 +10,7 @@ use phanerite_core::storage::ShareStrategy::Force;
 use phanerite_core::*;
 use std::num::NonZeroU8;
 use tracing::log::error;
-use tracing::{info, Level};
+use tracing::{Level, info};
 
 fn main() {
     tracing_subscriber::fmt().with_max_level(Level::INFO).init();
